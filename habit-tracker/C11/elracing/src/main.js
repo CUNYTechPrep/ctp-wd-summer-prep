04fe088c6ad9effe08666ef58172209d1d5450a4
elracing <55640950+elracing@users.noreply.github.com>
2025-08-22 07:24:17 +0000

    finished delivables for elracing, added CSS, calcutated streaks, implements clear()

--- habit-tracker/C11/elracing/src/main.js
+++ habit-tracker/C11/elracing/src/main.js
@@ -26,34 +26,42 @@ let habits = habitStorage.load() || [];
 
 const renderHabits = (habits) => {
   const habitlist = document.getElementById('habit_list')
+  const pointsDisplay = document.getElementById('pointsDisplay');
+  const points = calculatePoints();
   habitlist.innerHTML = ""; //clear list
+
+
 
   for (let i = 0; i < habits.length; i++) {
     const habit  = habits[i]
 
     const li = document.createElement('li')
-    li.textContent = `${habit.habitName}, category: ${habit.category}, target streak : ${habit.targetStreak}`
+    li.textContent = `${habit.habitName}, category: ${habit.category}, target streak : ${habit.targetStreak}, current streak : ${habit.completionStreak}`
     habitlist.appendChild(li)
 
   }
+
+  pointsDisplay.textContent = `Points: ${points}`; //replace points html content
+
 }
 
 renderHabits(habits);
 
-function calculatePoints(streak) {
-  return streak * 10;
+function calculatePoints() {
+  return calculateStreak() * 10;
 }
 
-function calculateStreak(dates) {
+function calculateStreak() {
     let count = 0;
-    for (let i = 0; i < dates.length; i++) {
-        count++;
+    for (let i = 0; i < habits.length; i++) {
+        count += habits[i].completionStreak;
     }
     
 
     return count;
 
 }
+
 
 function isHabitCompleted(habit, date) {
   let completed = false;
@@ -75,21 +83,44 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   
   const data = new FormData(event.target);
+  let repeatEntry = false; //flag to check for repeat habit
 
   console.log(Array.from(data.keys()));
 
   const habit = ({
-    habitName: data.get('habitName'),
+    habitName: data.get('habitName').trim().toLowerCase(), //normalize names
     targetStreak: data.get('targetStreak'),
     category: data.get('category'),
-    completions: []
+    completionStreak: 1 //initialize streak to 1
   });
 
-  habits.push(habit);
+  for (let i = 0; i < habits.length; i++){
+    if (habit.habitName === habits[i].habitName) {
+      habits[i].completionStreak++; //add to habit streak if habit already exists
+      repeatEntry = true;
+      break;
+    }
+  }
+  if (!repeatEntry) {
+    habits.push(habit);
+  }
 
   habitStorage.save(habits);
   renderHabits(habits);
 })
+
+
+const clearButton = document.getElementById('clearList');
+
+clearButton.addEventListener('click', (event) => {
+  habitStorage.clear();
+  renderHabits(habits);
+  location.reload(); //force refresh
+});
+
+
+
+
 
 
 
